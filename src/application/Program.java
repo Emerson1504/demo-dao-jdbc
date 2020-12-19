a36a0b4f940bd3c7946fc4f0f3463f4cdee51913
Emerson1504 <emrsilva1504@gmail.com>
2020-12-19 00:08:17 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -32,9 +32,15 @@ public class Program {
 		}
 
 		System.out.println("\n=== Test 4: seller insert ===");
-		Seller newSeller = new Seller (null, "Greg", "greg@gamail.com", new Date(), 4000.0, department);
+		Seller newSeller = new Seller (null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New Id =" + newSeller.getId()); 
+		
+		System.out.println("\n=== Test 5: seller insert ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 
 }
